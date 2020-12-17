8936abe4d424cc18520fb908c7e94c1e4a8e98c7
saxon <pengyuan@azalea-tech.com>
2020-12-17 17:49:57 +0800

    add home page

---
+++ src/components/aside.vue
@@ -0,0 +1,100 @@
+<template>
+  <a-layout class="layout-container">
+    <a-layout-sider :collapsed="collapsed"
+                    :trigger="null"
+                    collapsible>
+      <div class="logo" />
+      <a-menu :selected-keys="selectedKeys"
+              theme="dark"
+              mode="inline">
+        <a-menu-item key="1">
+          <user-outlined />
+          <span>nav 1</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <video-camera-outlined />
+          <span>nav 2</span>
+        </a-menu-item>
+        <a-menu-item key="3">
+          <upload-outlined />
+          <span>nav 3</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <a-layout-header style="background: #fff; padding: 0">
+        <menu-unfold-outlined v-if="collapsed"
+                              class="trigger"
+                              @click="() => (collapsed = !collapsed)" />
+        <menu-fold-outlined v-else
+                            class="trigger"
+                            @click="() => (collapsed = !collapsed)" />
+      </a-layout-header>
+      <a-layout-content :style="{ margin: '24px 16px',
+                                  padding: '24px', background: '#fff', minHeight: '280px' }">
+        Content
+      </a-layout-content>
+    </a-layout>
+  </a-layout>
+</template>
+
+<script>
+import {
+  UserOutlined,
+  VideoCameraOutlined,
+  UploadOutlined,
+  MenuUnfoldOutlined,
+  MenuFoldOutlined,
+} from '@ant-design/icons-vue';
+
+import { Menu, Layout } from 'ant-design-vue';
+
+export default {
+  components: {
+    UserOutlined,
+    VideoCameraOutlined,
+    UploadOutlined,
+    MenuUnfoldOutlined,
+    MenuFoldOutlined,
+
+    [Layout.name]: Layout,
+    [Layout.Sider.name]: Layout.Sider,
+    [Layout.Header.name]: Layout.Header,
+    [Layout.Content.name]: Layout.Content,
+
+    [Menu.name]: Menu,
+    [Menu.Item.name]: Menu.Item,
+    [Menu.SubMenu.name]: Menu.SubMenu,
+  },
+  data() {
+    return {
+      selectedKeys: ['1'],
+      collapsed: false,
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped>
+.layout-container {
+  height: 100%;
+
+  .trigger {
+    font-size: 18px;
+    line-height: 64px;
+    padding: 0 24px;
+    cursor: pointer;
+    transition: color 0.3s;
+
+    &:hover {
+      color: #1890ff;
+    }
+  }
+
+  .logo {
+    height: 32px;
+    background: rgba(255, 255, 255, 0.2);
+    margin: 16px;
+  }
+}
+</style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -1,25 +1,15 @@
 <template>
-  <a-badge dot>
-    <a-avatar :size="64">
-      U
-    </a-avatar>
-  </a-badge>
-  <h1>{{ msg }}</h1>
-  <a-button type="primary">
-    Primary
-  </a-button>
+  <Aside />
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import { Button, Avatar, Badge } from 'ant-design-vue';
+import Aside from '../components/aside.vue';
 
 export default defineComponent({
   name: 'Home',
   components: {
-    [Button.name]: Button,
-    [Avatar.name]: Avatar,
-    [Badge.name]: Badge,
+    Aside,
   },
   setup() {
     const msg = 'Welcome! vue3 + vite + ts';
